61c292da8cc50f672b2f902b8aa8cb42a7364b46
kephalosk <watchfromtheshore@gmail.com>
2025-05-04 17:33:29 +0200

    add component ContentContainer

---
+++ src/components/container/ContentContainer/ContentContainer.spec.tsx
@@ -0,0 +1,132 @@
+import SliderContainer from "@/components/container/SliderContainer/SliderContainer.tsx";
+import OptionContainer from "@/components/container/OptionContainer/OptionContainer.tsx";
+import StrengthContainer from "@/components/container/StrengthContainer/StrengthContainer.tsx";
+import Button from "@/components/atoms/Button/Button.tsx";
+import { ReactElement } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import ContentContainer from "@/components/container/ContentContainer/ContentContainer.tsx";
+import { BUTTON_TEXT } from "@/globals/constants/constants.ts";
+
+const sliderContainerDataTestId: string = "slider-container";
+jest.mock(
+  "@/components/container/SliderContainer/SliderContainer.tsx",
+  (): jest.Mock =>
+    jest.fn((): ReactElement => {
+      return <div data-testid={sliderContainerDataTestId}></div>;
+    }),
+);
+
+const optionContainerDataTestId: string = "option-container";
+jest.mock(
+  "@/components/container/OptionContainer/OptionContainer.tsx",
+  (): jest.Mock =>
+    jest.fn((): ReactElement => {
+      return <div data-testid={optionContainerDataTestId}></div>;
+    }),
+);
+
+const strengthContainerDataTestId: string = "strength-container";
+jest.mock(
+  "@/components/container/StrengthContainer/StrengthContainer.tsx",
+  (): jest.Mock =>
+    jest.fn((): ReactElement => {
+      return <div data-testid={strengthContainerDataTestId}></div>;
+    }),
+);
+
+const buttonDataTestId: string = "button";
+jest.mock(
+  "@/components/atoms/Button/Button.tsx",
+  (): jest.Mock =>
+    jest.fn((props): ReactElement => {
+      return (
+        <div
+          data-testid={buttonDataTestId}
+          onClick={props.handleButtonClick}
+        ></div>
+      );
+    }),
+);
+
+describe("ContentContainer Component", (): void => {
+  const setup = (): { container: HTMLElement } => {
+    return render(<ContentContainer />);
+  };
+
+  const contentContainerSelecter: string = "contentContainer";
+
+  it(`renders div ${contentContainerSelecter}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${contentContainerSelecter}`,
+    );
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders component SliderContainer", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(sliderContainerDataTestId);
+
+    expect(element).toBeInTheDocument();
+    expect(SliderContainer).toHaveBeenCalledTimes(1);
+    expect(SliderContainer).toHaveBeenCalledWith({}, undefined);
+  });
+
+  it("renders component OptionContainer", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(optionContainerDataTestId);
+
+    expect(element).toBeInTheDocument();
+    expect(OptionContainer).toHaveBeenCalledTimes(1);
+    expect(OptionContainer).toHaveBeenCalledWith({}, undefined);
+  });
+
+  it("renders component StrengthContainer", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(
+      strengthContainerDataTestId,
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(StrengthContainer).toHaveBeenCalledTimes(1);
+    expect(StrengthContainer).toHaveBeenCalledWith({}, undefined);
+  });
+
+  it("renders component Button", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(buttonDataTestId);
+
+    expect(element).toBeInTheDocument();
+    expect(Button).toHaveBeenCalledTimes(1);
+    expect(Button).toHaveBeenCalledWith(
+      {
+        handleButtonClick: expect.any(Function),
+        text: BUTTON_TEXT,
+      },
+      undefined,
+    );
+  });
+
+  it("calls handleButtonClick", (): void => {
+    const handleButtonClickMock: jest.Mock = jest.fn();
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(buttonDataTestId);
+    element.onclick = handleButtonClickMock;
+    fireEvent.click(element);
+
+    expect(handleButtonClickMock).toHaveBeenCalledTimes(1);
+    expect(handleButtonClickMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        type: "click",
+        target: element,
+      }),
+    );
+  });
+});
---
+++ src/components/container/ContentContainer/ContentContainer.tsx
@@ -0,0 +1,24 @@
+import "./ContentContainer.scss";
+import SliderContainer from "@/components/container/SliderContainer/SliderContainer.tsx";
+import OptionContainer from "@/components/container/OptionContainer/OptionContainer.tsx";
+import StrengthContainer from "@/components/container/StrengthContainer/StrengthContainer.tsx";
+import Button from "@/components/atoms/Button/Button.tsx";
+import { BUTTON_TEXT } from "@/globals/constants/constants.ts";
+
+const ContentContainer = () => {
+  return (
+    <div className="contentContainer">
+      <SliderContainer />
+      <OptionContainer />
+      <StrengthContainer />
+      <Button
+        text={BUTTON_TEXT}
+        handleButtonClick={function (): void {
+          console.log("Function not implemented.");
+        }}
+      />
+    </div>
+  );
+};
+
+export default ContentContainer;
