97d6f01d49ed8a7725ee1d41cedec3d50c0b8568
kephalosk <watchfromtheshore@gmail.com>
2025-05-04 19:01:22 +0200

    add atom component Footer

---
+++ src/components/atoms/Footer/Footer.spec.tsx
@@ -0,0 +1,83 @@
+import { render } from "@testing-library/react";
+import Footer from "@/components/atoms/Footer/Footer.tsx";
+import {
+  FRONTEND_MENTOR_NAME,
+  FRONTEND_MENTOR_PREFIX,
+  FRONTEND_MENTOR_SUFFIX,
+  GITHUB_PROFILE_NAME,
+} from "@/globals/constants/constants.ts";
+import {
+  FRONTEND_MENTOR_SRC,
+  GITHUB_SRC,
+} from "@/globals/constants/ressources.ts";
+
+describe("Footer Component", (): void => {
+  const setup = (): { container: HTMLElement } => {
+    return render(<Footer />);
+  };
+
+  const attributionSelector: string = "attribution";
+  const attributionPrefixSelector: string = "attributionPrefix";
+  const attributionFrontendMentor: string = "attributionFrontendMentor";
+  const attributionSuffixSelector: string = "attributionSuffix";
+  const attributionPersonalProfileSelector: string =
+    "attributionPersonalProfile";
+
+  it(`renders footer ${attributionSelector}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${attributionSelector}`,
+    );
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it(`renders span ${attributionPrefixSelector}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${attributionPrefixSelector}`,
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element!.innerHTML).toEqual(FRONTEND_MENTOR_PREFIX);
+  });
+
+  it(`renders a ${attributionFrontendMentor}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${attributionFrontendMentor}`,
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("href", FRONTEND_MENTOR_SRC);
+    expect(element).toHaveAttribute("target", "_blank");
+    expect(element!.innerHTML).toEqual(FRONTEND_MENTOR_NAME);
+  });
+
+  it(`renders span ${attributionSuffixSelector}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${attributionSuffixSelector}`,
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element!.innerHTML).toEqual(FRONTEND_MENTOR_SUFFIX);
+  });
+
+  it(`renders a ${attributionPersonalProfileSelector}`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${attributionPersonalProfileSelector}`,
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("href", GITHUB_SRC);
+    expect(element).toHaveAttribute("target", "_blank");
+    expect(element!.innerHTML).toEqual(GITHUB_PROFILE_NAME);
+  });
+});
---
+++ src/components/atoms/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import "./Footer.scss";
+import React, { ReactElement } from "react";
+import {
+  FRONTEND_MENTOR_SRC,
+  GITHUB_SRC,
+} from "@/globals/constants/ressources.ts";
+import {
+  FRONTEND_MENTOR_NAME,
+  FRONTEND_MENTOR_PREFIX,
+  FRONTEND_MENTOR_SUFFIX,
+  GITHUB_PROFILE_NAME,
+} from "@/globals/constants/constants.ts";
+
+const Footer: React.FC = (): ReactElement => {
+  return (
+    <footer className="attribution">
+      <span className="attributionPrefix">{FRONTEND_MENTOR_PREFIX}</span>
+      <a
+        className="attributionFrontendMentor"
+        href={FRONTEND_MENTOR_SRC}
+        target="_blank"
+      >
+        {FRONTEND_MENTOR_NAME}
+      </a>
+      <span className="attributionSuffix">{FRONTEND_MENTOR_SUFFIX}</span>
+      <a
+        className="attributionPersonalProfile"
+        href={GITHUB_SRC}
+        target="_blank"
+      >
+        {GITHUB_PROFILE_NAME}
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
