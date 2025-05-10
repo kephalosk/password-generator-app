6731abf216c23e78178af40e7fa70824bcd0bf79
kephalosk <watchfromtheshore@gmail.com>
2025-05-10 12:00:50 +0200

    add subhook useSliderMouseEventMouseMove

---
+++ src/hooks/slider/subhooks/useSliderMouseEventMouseMove.spec.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { SliderMouseMouseMoveHook } from "@/globals/types/SliderMouseTypes.ts";
+import useSliderMouseEventMouseMove from "@/hooks/slider/subhooks/useSliderMouseEventMouseMove.ts";
+
+const clearTimeoutIdMock: jest.Mock = jest.fn();
+const updatePositionMock: jest.Mock = jest.fn();
+const setTimeoutIdMock: jest.Mock = jest.fn();
+
+const testComponentDataTestId: string = "test-component";
+const TestComponent: React.FC<{ isDragging: boolean }> = ({ isDragging }) => {
+  const { handleMouseMove }: SliderMouseMouseMoveHook =
+    useSliderMouseEventMouseMove(
+      isDragging,
+      clearTimeoutIdMock,
+      updatePositionMock,
+      setTimeoutIdMock,
+    );
+
+  return (
+    <div
+      data-testid={testComponentDataTestId}
+      onMouseMove={handleMouseMove}
+    ></div>
+  );
+};
+
+describe("useSliderMouseEventMouseMove Hook", (): void => {
+  const setup = (props: { isDragging: boolean }): void => {
+    render(<TestComponent isDragging={props.isDragging} />);
+  };
+
+  beforeAll((): void => {
+    jest.useFakeTimers();
+  });
+
+  afterAll((): void => {
+    jest.useRealTimers();
+  });
+
+  it("does not call clearTimeoutId, updatePosition, setTimeoutId if isDragging is false", (): void => {
+    setup({ isDragging: false });
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.mouseMove(element);
+
+    expect(clearTimeoutIdMock).not.toHaveBeenCalled();
+    expect(updatePositionMock).not.toHaveBeenCalled();
+    expect(setTimeoutIdMock).not.toHaveBeenCalled();
+  });
+
+  it("calls clearTimeoutId, updatePosition, setTimeoutId if isDragging is true", (): void => {
+    const expectedTimeoutId: number = 1000000000000;
+    const onMouseMoveEvent: string = "onMouseMove";
+    setup({ isDragging: true });
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.mouseMove(element);
+
+    jest.runAllTimers();
+
+    expect(clearTimeoutIdMock).toHaveBeenCalledTimes(1);
+    expect(clearTimeoutIdMock).toHaveBeenCalledWith();
+    expect(updatePositionMock).toHaveBeenCalled();
+    expect(updatePositionMock).toHaveBeenCalledWith(
+      expect.objectContaining({ _reactName: onMouseMoveEvent }),
+    );
+    expect(setTimeoutIdMock).toHaveBeenCalledTimes(1);
+    expect(setTimeoutIdMock).toHaveBeenCalledWith(expectedTimeoutId);
+  });
+});
---
+++ src/hooks/slider/subhooks/useSliderMouseEventMouseMove.ts
@@ -0,0 +1,28 @@
+import React, { useCallback } from "react";
+import { SliderMouseMouseMoveHook } from "@/globals/types/SliderMouseTypes.ts";
+
+const useSliderMouseEventMouseMove = (
+  isDragging: boolean,
+  clearTimeoutId: () => void,
+  updatePosition: (event: MouseEvent | React.MouseEvent) => void,
+  setTimeoutId: React.Dispatch<React.SetStateAction<NodeJS.Timeout | null>>,
+): SliderMouseMouseMoveHook => {
+  const handleMouseMove = useCallback(
+    (event: MouseEvent | React.MouseEvent): void => {
+      if (isDragging) {
+        clearTimeoutId();
+
+        const newTimeoutId: NodeJS.Timeout = setTimeout((): void => {
+          updatePosition(event);
+        }, 10);
+
+        setTimeoutId(newTimeoutId);
+      }
+    },
+    [clearTimeoutId, isDragging, setTimeoutId, updatePosition],
+  );
+
+  return { handleMouseMove };
+};
+
+export default useSliderMouseEventMouseMove;
