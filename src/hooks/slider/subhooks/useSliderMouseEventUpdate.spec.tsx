3a1ead49420ea40547685d6a0dc694cac173ee1a
kephalosk <watchfromtheshore@gmail.com>
2025-05-10 13:15:24 +0200

    refactor unit tests of subhook useSliderMouseEventUpdate

--- src/hooks/slider/subhooks/useSliderMouseEventUpdate.spec.tsx
+++ src/hooks/slider/subhooks/useSliderMouseEventUpdate.spec.tsx
@@ -1,60 +1,145 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { SliderMouseMouseUpHook } from "@/globals/types/SliderMouseTypes.ts";
-import useSliderMouseEventMouseUp from "@/hooks/slider/subhooks/useSliderMouseEventMouseUp.ts";
+import { SliderMouseUpdateHook } from "@/globals/types/SliderMouseTypes.ts";
+import useSliderMouseEventUpdate from "@/hooks/slider/subhooks/useSliderMouseEventUpdate.ts";
+import {
+  CHARACTER_LENGTH_MAX_VALUE,
+  CHARACTER_LENGTH_MIN_VALUE,
+} from "@/globals/config.ts";
+import correctValuesThatAreTooSmall from "@/globals/utils/correctValuesThatAreTooSmall.ts";
+import getNewPositionAbsolute from "@/globals/helper/slider/getNewPositionAbsolute.ts";
+import isNewPositionValid from "@/globals/utils/isNewPositionValid.ts";
+import { ERROR_MESSAGE_SLIDER_MOUSE_UPDATE_PREFIX } from "@/globals/constants/ErrorMessages.ts";
 
-const onDraggingChangeMock: jest.Mock = jest.fn();
-const clearTimeoutIdMock: jest.Mock = jest.fn();
+jest.spyOn(console, "error").mockImplementation((): null => null);
+
+const correctValuesThatAreTooSmallMockValue: number =
+  CHARACTER_LENGTH_MIN_VALUE;
+jest.mock(
+  "@/globals/utils/correctValuesThatAreTooSmall.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn((): number => correctValuesThatAreTooSmallMockValue),
+  }),
+);
+
+const getNewPositionAbsoluteMockValue: number = CHARACTER_LENGTH_MIN_VALUE;
+jest.mock(
+  "@/globals/helper/slider/getNewPositionAbsolute.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn((): number => getNewPositionAbsoluteMockValue),
+  }),
+);
+
+const isNewPositionValidMockValue: boolean = true;
+jest.mock(
+  "@/globals/utils/isNewPositionValid.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn((): boolean => isNewPositionValidMockValue),
+  }),
+);
+
+const minValue: number = CHARACTER_LENGTH_MIN_VALUE;
+const maxValue: number = CHARACTER_LENGTH_MAX_VALUE;
+const propagateNewValueMock: jest.Mock = jest.fn();
+const onPositionChangeMock: jest.Mock = jest.fn();
 
 const testComponentDataTestId: string = "test-component";
 const TestComponent: React.FC = () => {
-  const { handleMouseUp }: SliderMouseMouseUpHook = useSliderMouseEventMouseUp(
-      onDraggingChangeMock,
-      clearTimeoutIdMock,
-  );
+  const { handleClick, containerRef, updatePosition }: SliderMouseUpdateHook =
+    useSliderMouseEventUpdate(
+      minValue,
+      maxValue,
+      propagateNewValueMock,
+      onPositionChangeMock,
+    );
 
   return (
-      <div data-testid={testComponentDataTestId} onMouseUp={handleMouseUp}></div>
-);
+    <div
+      ref={containerRef}
+      data-testid={testComponentDataTestId}
+      onClick={handleClick}
+      onMouseMove={updatePosition}
+    ></div>
+  );
 };
 
-describe("useSliderMouseEventMouseUp Hook", (): void => {
+describe("useSliderMouseEventUpdate Hook", (): void => {
   const setup = (): void => {
     render(<TestComponent />);
   };
 
-  it("does not call onDraggingChange by default", (): void => {
+  it("does not call updatePosition by default", (): void => {
     setup();
 
-    expect(onDraggingChangeMock).not.toHaveBeenCalled();
+    expect(getNewPositionAbsolute).not.toHaveBeenCalled();
+    expect(correctValuesThatAreTooSmall).not.toHaveBeenCalled();
+    expect(isNewPositionValid).not.toHaveBeenCalled();
+    expect(propagateNewValueMock).not.toHaveBeenCalled();
+    expect(onPositionChangeMock).not.toHaveBeenCalled();
   });
 
-  it("does not call clearTimeoutId by default", (): void => {
-    setup();
-
-    expect(clearTimeoutIdMock).not.toHaveBeenCalled();
-  });
-
-  it("calls onDraggingChange with false when handleMouseUp is triggered", (): void => {
-    const expectedDraggingChangeBoolean: boolean = false;
+  it("calls updatePosition when handleClick is triggered", (): void => {
     setup();
 
     const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
-    fireEvent.mouseUp(element);
+    fireEvent.click(element);
 
-    expect(onDraggingChangeMock).toHaveBeenCalledTimes(1);
-    expect(onDraggingChangeMock).toHaveBeenCalledWith(
-        expectedDraggingChangeBoolean,
+    expect(getNewPositionAbsolute).toHaveBeenCalled();
+    expect(correctValuesThatAreTooSmall).toHaveBeenCalled();
+    expect(isNewPositionValid).toHaveBeenCalled();
+    expect(propagateNewValueMock).toHaveBeenCalled();
+    expect(onPositionChangeMock).toHaveBeenCalled();
+  });
+
+  it("calls updatePosition directly", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.mouseMove(element);
+
+    expect(getNewPositionAbsolute).toHaveBeenCalled();
+    expect(correctValuesThatAreTooSmall).toHaveBeenCalled();
+    expect(isNewPositionValid).toHaveBeenCalled();
+    expect(propagateNewValueMock).toHaveBeenCalled();
+    expect(onPositionChangeMock).toHaveBeenCalled();
+  });
+
+  it("returns isNewPositionValid is false", (): void => {
+    (isNewPositionValid as jest.Mock).mockReturnValue(false);
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.click(element);
+
+    expect(propagateNewValueMock).not.toHaveBeenCalled();
+    expect(onPositionChangeMock).not.toHaveBeenCalled();
+  });
+
+  it("logs errors", (): void => {
+    (isNewPositionValid as jest.Mock).mockImplementation((): void => {
+      throw new Error();
+    });
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.click(element);
+
+    expect(console.error).toHaveBeenCalledTimes(1);
+    expect(console.error).toHaveBeenCalledWith(
+      ERROR_MESSAGE_SLIDER_MOUSE_UPDATE_PREFIX,
+      expect.any(Error),
     );
-  });
-
-  it("calls clearTimeoutId hen handleMouseUp is triggered", (): void => {
-    setup();
-
-    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
-    fireEvent.mouseUp(element);
-
-    expect(clearTimeoutIdMock).toHaveBeenCalledTimes(1);
-    expect(clearTimeoutIdMock).toHaveBeenCalledWith();
   });
 });
