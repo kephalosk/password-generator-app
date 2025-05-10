3575d60937ec7ca2ac4c50453e7dafba9488853f
kephalosk <watchfromtheshore@gmail.com>
2025-05-10 12:03:45 +0200

    add subhook useSliderMouseEventMouseUp

---
+++ src/hooks/slider/subhooks/useSliderMouseEventMouseUp.spec.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { SliderMouseMouseUpHook } from "@/globals/types/SliderMouseTypes.ts";
+import useSliderMouseEventMouseUp from "@/hooks/slider/subhooks/useSliderMouseEventMouseUp.ts";
+
+const onDraggingChangeMock: jest.Mock = jest.fn();
+const clearTimeoutIdMock: jest.Mock = jest.fn();
+
+const testComponentDataTestId: string = "test-component";
+const TestComponent: React.FC = () => {
+  const { handleMouseUp }: SliderMouseMouseUpHook = useSliderMouseEventMouseUp(
+    onDraggingChangeMock,
+    clearTimeoutIdMock,
+  );
+
+  return (
+    <div data-testid={testComponentDataTestId} onMouseUp={handleMouseUp}></div>
+  );
+};
+
+describe("useSliderMouseEventMouseUp Hook", (): void => {
+  const setup = (): void => {
+    render(<TestComponent />);
+  };
+
+  it("does not call onDraggingChange by default", (): void => {
+    setup();
+
+    expect(onDraggingChangeMock).not.toHaveBeenCalled();
+  });
+
+  it("does not call clearTimeoutId by default", (): void => {
+    setup();
+
+    expect(clearTimeoutIdMock).not.toHaveBeenCalled();
+  });
+
+  it("calls onDraggingChange with false when handleMouseUp is triggered", (): void => {
+    const expectedDraggingChangeBoolean: boolean = false;
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.mouseUp(element);
+
+    expect(onDraggingChangeMock).toHaveBeenCalledTimes(1);
+    expect(onDraggingChangeMock).toHaveBeenCalledWith(
+      expectedDraggingChangeBoolean,
+    );
+  });
+
+  it("calls clearTimeoutId hen handleMouseUp is triggered", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(testComponentDataTestId);
+    fireEvent.mouseUp(element);
+
+    expect(clearTimeoutIdMock).toHaveBeenCalledTimes(1);
+    expect(clearTimeoutIdMock).toHaveBeenCalledWith();
+  });
+});
---
+++ src/hooks/slider/subhooks/useSliderMouseEventMouseUp.ts
@@ -0,0 +1,16 @@
+import { useCallback } from "react";
+import { SliderMouseMouseUpHook } from "@/globals/types/SliderMouseTypes.ts";
+
+const useSliderMouseEventMouseUp = (
+  onDraggingChange: (newDragging: boolean) => void,
+  clearTimeoutId: () => void,
+): SliderMouseMouseUpHook => {
+  const handleMouseUp = useCallback((): void => {
+    onDraggingChange(false);
+    clearTimeoutId();
+  }, [clearTimeoutId, onDraggingChange]);
+
+  return { handleMouseUp };
+};
+
+export default useSliderMouseEventMouseUp;
