d1d714f78318a8467b8b31f029a3891e04a50053
kephalosk <watchfromtheshore@gmail.com>
2025-05-11 15:16:42 +0200

    add function getStrengthLevelFromPoints

---
+++ src/globals/helper/password/getStrengthLevelFromPoints.spec.ts
@@ -0,0 +1,21 @@
+import { SecurityLevelEnum } from "@/globals/models/enums/SecurityLevelEnum.ts";
+import getStrengthLevelFromPoints from "@/globals/helper/password/getStrengthLevelFromPoints.ts";
+
+describe("getStrengthLevelFromPoints function", (): void => {
+  it.each([
+    [SecurityLevelEnum.NONE, 0],
+    [SecurityLevelEnum.WEAK, 1],
+    [SecurityLevelEnum.WEAK, 2],
+    [SecurityLevelEnum.WEAK, 3],
+    [SecurityLevelEnum.LOW, 4],
+    [SecurityLevelEnum.MEDIUM, 5],
+    [SecurityLevelEnum.STRONG, 6],
+  ])(
+    "returns Securitylevel %s for %s points",
+    (level: SecurityLevelEnum, points: number): void => {
+      const result: SecurityLevelEnum = getStrengthLevelFromPoints(points);
+
+      expect(result).toEqual(level);
+    },
+  );
+});
