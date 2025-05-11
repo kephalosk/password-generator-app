4dd41d95e5170c49be3319a596aa8d3fd0ca453b
kephalosk <watchfromtheshore@gmail.com>
2025-05-11 14:44:39 +0200

    add function getPointsFromLength

--- src/globals/config.ts
+++ src/globals/config.ts
@@ -1,3 +1,6 @@
 export const CHARACTER_LENGTH_MIN_VALUE: number = 4;
 export const CHARACTER_LENGTH_MAX_VALUE: number = 30;
 export const OFFSET_ADJUSTER: number = 14;
+
+export const SHORT_STRING_LENGTH: number = 8;
+export const LONG_STRING_LENGTH: number = 16;
