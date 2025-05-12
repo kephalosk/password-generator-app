6d42ceeb614ca92afaf575615a0f94cd358726f5
kephalosk <watchfromtheshore@gmail.com>
2025-05-12 14:12:13 +0200

    update PasswordContainer

--- src/components/container/PasswordContainer/PasswordContainer.spec.tsx
+++ src/components/container/PasswordContainer/PasswordContainer.spec.tsx
@@ -1,14 +1,66 @@
 import Label from "@/components/atoms/Label/Label.tsx";
 import { ReactElement } from "react";
-import { render } from "@testing-library/react";
-import PasswordContainer from "@/components/container/PasswordContainer/PasswordContainer.tsx";
+import { fireEvent, render } from "@testing-library/react";
+import PasswordContainer, {
+  PasswordContainerProps,
+} from "@/components/container/PasswordContainer/PasswordContainer.tsx";
 import { LabelTypeEnum } from "@/globals/models/enums/LabelTypeEnum.ts";
-import { COPY_ICON_SRC } from "@/globals/constants/Ressources.ts";
+import { COPY_ICON_D, COPY_ICON_SRC } from "@/globals/constants/Ressources.ts";
 import {
   COPY_ICON_ALT_TEXT,
   COPY_TEXT,
-  EMPTY_STRING,
 } from "@/globals/constants/Constants.ts";
+import usePasswordCopy from "@/hooks/password/usePasswordCopy.ts";
+import useCopyStatus from "@/hooks/password/useCopyStatus.ts";
+import useBlurOnPointerUp from "@/hooks/button/useBlurOnPointerUp.ts";
+import useKeyClickBypass from "@/hooks/button/useKeyClickBypass.ts";
+import { PasswordCopyHook } from "@/globals/models/types/PasswordCopyTypes.ts";
+import { KeyClickBypassHook } from "@/globals/models/types/KeyClickBypassTypes.ts";
+import { ReactNamesEnum } from "@/globals/models/enums/ReactNamesEnum.ts";
+
+jest.mock(
+  "@/hooks/password/usePasswordCopy.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/password/useCopyStatus.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/button/useBlurOnPointerUp.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/button/useKeyClickBypass.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
 
 const labelDataTestId: string = "label";
 jest.mock(
@@ -19,16 +71,60 @@ jest.mock(
     }),
 );
 
-describe("", (): void => {
-  const setup = (): { container: HTMLElement } => {
-    return render(<PasswordContainer />);
+describe("PasswordContainer component", (): void => {
+  const password: string = "password";
+
+  const setup = (
+    propsOverride?: Partial<PasswordContainerProps>,
+  ): { container: HTMLElement } => {
+    const defaultProps: PasswordContainerProps = {
+      password,
+    };
+
+    const props: PasswordContainerProps = { ...defaultProps, ...propsOverride };
+    return render(<PasswordContainer {...props} />);
   };
+
+  const isCopiedMock: boolean = false;
+  const setIsCopiedMock: jest.Mock = jest.fn();
+  const handleButtonClickMock: jest.Mock = jest.fn();
+  const usePasswordCopyMock: PasswordCopyHook = {
+    isCopied: isCopiedMock,
+    setIsCopied: setIsCopiedMock,
+    handleButtonClick: handleButtonClickMock,
+  };
+
+  const handlePointerUpMock: jest.Mock = jest.fn();
+
+  const handleClickMock: jest.Mock = jest.fn();
+  const handleKeyDownMock: jest.Mock = jest.fn();
+  const useKeyClickBypassMock: KeyClickBypassHook = {
+    handleClick: handleClickMock,
+    handleKeyDown: handleKeyDownMock,
+  };
+
+  beforeEach((): void => {
+    (usePasswordCopy as jest.Mock).mockReturnValue(usePasswordCopyMock);
+    (useCopyStatus as jest.Mock).mockReturnValue(undefined);
+    (useBlurOnPointerUp as jest.Mock).mockReturnValue(handlePointerUpMock);
+    (useKeyClickBypass as jest.Mock).mockReturnValue(useKeyClickBypassMock);
+  });
 
   it("renders div passwordContainer", (): void => {
     const { container } = setup();
 
     const element: HTMLElement | null =
       container.querySelector(".passwordContainer");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders div passwordContainerSpaceWrapper", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerSpaceWrapper",
+    );
 
     expect(element).toBeInTheDocument();
   });
@@ -51,15 +147,29 @@ describe("", (): void => {
     );
 
     expect(element).toBeInTheDocument();
-    expect(Label).toHaveBeenCalledTimes(2);
+    expect(Label).toHaveBeenCalledTimes(1);
     expect(Label).toHaveBeenNthCalledWith(
       1,
-      { text: EMPTY_STRING, type: LabelTypeEnum.PASSWORD_LABEL },
+      { text: password, type: LabelTypeEnum.PASSWORD_LABEL },
       undefined,
     );
   });
 
+  it("renders div passwordContainerSpaceCopy", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerSpaceCopy",
+    );
+
+    expect(element).toBeInTheDocument();
+  });
+
   it("renders component Label for copy", (): void => {
+    (usePasswordCopy as jest.Mock).mockReturnValue({
+      ...usePasswordCopyMock,
+      isCopied: true,
+    });
     const { container } = setup();
 
     const element: HTMLElement | null = container.querySelector(
@@ -75,7 +185,76 @@ describe("", (): void => {
     );
   });
 
-  it("renders img passwordContainerIcon", (): void => {
+  it("does not render component Label for copy when isCopied is false", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      `.${LabelTypeEnum.COPY_LABEL}`,
+    );
+
+    expect(element).not.toBeInTheDocument();
+    expect(Label).toHaveBeenCalledTimes(1);
+  });
+
+  it("renders div passwordContainerIconWrapper", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconWrapper",
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("aria-label", COPY_ICON_ALT_TEXT);
+    expect(element).toHaveAttribute("aria-hidden", "false");
+    expect(element).toHaveAttribute("tabIndex", "0");
+  });
+
+  it("calls handleClick when passwordContainerIconWrapper is clicked", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconWrapper",
+    );
+    fireEvent.click(element!);
+
+    expect(element).toBeInTheDocument();
+    expect(handleClickMock).toHaveBeenCalledTimes(1);
+    expect(handleClickMock).toHaveBeenCalledWith(
+      expect.objectContaining({ _reactName: ReactNamesEnum.ON_CLICK }),
+    );
+  });
+
+  it("calls handleKeyDown when passwordContainerIconWrapper is keydowned", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconWrapper",
+    );
+    fireEvent.keyDown(element!);
+
+    expect(element).toBeInTheDocument();
+    expect(handleKeyDownMock).toHaveBeenCalledTimes(1);
+    expect(handleKeyDownMock).toHaveBeenCalledWith(
+      expect.objectContaining({ _reactName: ReactNamesEnum.ON_KEY_DOWN }),
+    );
+  });
+
+  it("calls handlePointerUp when passwordContainerIconWrapper is mousedowned", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconWrapper",
+    );
+    fireEvent.mouseDown(element!);
+
+    expect(element).toBeInTheDocument();
+    expect(handlePointerUpMock).toHaveBeenCalledTimes(1);
+    expect(handlePointerUpMock).toHaveBeenCalledWith(
+      expect.objectContaining({ _reactName: ReactNamesEnum.ON_MOUSE_DOWN }),
+    );
+  });
+
+  it("renders svg passwordContainerIcon", (): void => {
     const { container } = setup();
 
     const element: HTMLElement | null = container.querySelector(
@@ -83,10 +262,51 @@ describe("", (): void => {
     );
 
     expect(element).toBeInTheDocument();
-    expect(element).toHaveAttribute("src", COPY_ICON_SRC);
-    expect(element).toHaveAttribute("alt", COPY_ICON_ALT_TEXT);
-    expect(element).toHaveAttribute("aria-label", COPY_ICON_ALT_TEXT);
-    expect(element).toHaveAttribute("aria-hidden", "false");
-    expect(element).toHaveAttribute("tabIndex", "0");
+    expect(element).toHaveAttribute("xmlns", COPY_ICON_SRC);
+  });
+
+  it("renders path passwordContainerIconPath", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconPath",
+    );
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveAttribute("d", COPY_ICON_D);
+  });
+
+  it("calls hook usePasswordCopy", (): void => {
+    setup();
+
+    expect(usePasswordCopy).toHaveBeenCalledTimes(1);
+    expect(usePasswordCopy).toHaveBeenCalledWith(password);
+  });
+
+  it("calls hook useCopyStatus", (): void => {
+    setup();
+
+    expect(useCopyStatus).toHaveBeenCalledTimes(1);
+    expect(useCopyStatus).toHaveBeenCalledWith(password, setIsCopiedMock);
+  });
+
+  it("calls hook useBlurOnPointerUp", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".passwordContainerIconWrapper",
+    );
+
+    expect(useBlurOnPointerUp).toHaveBeenCalledTimes(1);
+    expect(useBlurOnPointerUp).toHaveBeenCalledWith({
+      current: element,
+    });
+  });
+
+  it("calls hook useKeyClickBypass", (): void => {
+    setup();
+
+    expect(useKeyClickBypass).toHaveBeenCalledTimes(1);
+    expect(useKeyClickBypass).toHaveBeenCalledWith(handleButtonClickMock);
   });
 });
--- src/components/container/PasswordContainer/PasswordContainer.tsx
+++ src/components/container/PasswordContainer/PasswordContainer.tsx
@@ -4,25 +4,57 @@ import { LabelTypeEnum } from "@/globals/models/enums/LabelTypeEnum.ts";
 import {
   COPY_ICON_ALT_TEXT,
   COPY_TEXT,
-  EMPTY_STRING,
 } from "@/globals/constants/Constants.ts";
-import { COPY_ICON_SRC } from "@/globals/constants/Ressources.ts";
+import { COPY_ICON_D, COPY_ICON_SRC } from "@/globals/constants/Ressources.ts";
+import React, { useRef } from "react";
+import useKeyClickBypass from "@/hooks/button/useKeyClickBypass.ts";
+import useBlurOnPointerUp from "@/hooks/button/useBlurOnPointerUp.ts";
+import usePasswordCopy from "@/hooks/password/usePasswordCopy.ts";
+import useCopyStatus from "@/hooks/password/useCopyStatus.ts";
+import { PasswordCopyHook } from "@/globals/models/types/PasswordCopyTypes.ts";
 
-const PasswordContainer = () => {
+export interface PasswordContainerProps {
+  password: string;
+}
+
+const PasswordContainer = ({ password }: PasswordContainerProps) => {
+  const { isCopied, setIsCopied, handleButtonClick }: PasswordCopyHook =
+    usePasswordCopy(password);
+  useCopyStatus(password, setIsCopied);
+
+  const divRef: React.RefObject<HTMLDivElement | null> =
+    useRef<HTMLDivElement>(null);
+  const handlePointerUp = useBlurOnPointerUp(divRef);
+  const { handleClick, handleKeyDown } = useKeyClickBypass(handleButtonClick);
+
   return (
     <div className="passwordContainer">
-      <span className="passwordContainerSpace">
-        <Label type={LabelTypeEnum.PASSWORD_LABEL} text={EMPTY_STRING} />
-      </span>
-      <Label type={LabelTypeEnum.COPY_LABEL} text={COPY_TEXT} />
-      <img
-        className="passwordContainerIcon"
-        src={COPY_ICON_SRC}
-        alt={COPY_ICON_ALT_TEXT}
+      <div className="passwordContainerSpaceWrapper">
+        <span className="passwordContainerSpace">
+          <Label type={LabelTypeEnum.PASSWORD_LABEL} text={password} />
+        </span>
+        <div className="passwordContainerSpaceCopy">
+          {isCopied && (
+            <Label type={LabelTypeEnum.COPY_LABEL} text={COPY_TEXT} />
+          )}
+        </div>
+      </div>
+      <div
+        ref={divRef}
+        className="passwordContainerIconWrapper"
+        tabIndex={0}
+        onClick={handleClick}
+        onKeyDown={(event: React.KeyboardEvent<HTMLDivElement>) =>
+          handleKeyDown(event)
+        }
+        onMouseDown={handlePointerUp}
         aria-label={COPY_ICON_ALT_TEXT}
         aria-hidden={false}
-        tabIndex={0}
-      />
+      >
+        <svg className="passwordContainerIcon" xmlns={COPY_ICON_SRC}>
+          <path className="passwordContainerIconPath" d={COPY_ICON_D} />
+        </svg>
+      </div>
     </div>
   );
 };
